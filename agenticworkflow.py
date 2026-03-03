ef5f0ee55bdfd30fd9d0caf9f839059c3047863e
Anton Backdahl <anton.backdahl@gmail.com>
2026-03-03 15:23:17 +0100

    added llamma 3.1 to the main script

--- agenticworkflow.py
+++ agenticworkflow.py
@@ -1,5 +1,12 @@
+from langchain_ollama import ChatOllama
+
 def main():
-    print("Hello World!")
+    model = ChatOllama(model="llama3.1:8b", temperature=100)
+    response = model.invoke("Hello my good sir")
+    print(response.content)
+
+
+    
 
 
 if __name__ == "__main__":
